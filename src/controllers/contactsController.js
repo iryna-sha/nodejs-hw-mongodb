d2b109a3f9aea50e5fca76c3f9699a39f99f490e
iryna-sha <154839228+iryna-sha@users.noreply.github.com>
2024-10-20 21:57:33 +0200

    Update contactsController.js
    123

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,36 +1,75 @@
-import { model, Schema } from 'mongoose';
+import {
+  createContact,
+  deleteContact,
+  getAllContacts,
+  getContactById,
+  updateContact,
+} from '../services/contacts.js';
+import createHttpError from 'http-errors';
 
-const contactSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
+export const getContactsController = async (_, res, next) => {
+  const contacts = await getAllContacts();
+  res.json({
+    status: 200,
+    message: 'Sucessfully found contacts!',
+    data: contacts,
+  });
+};
 
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  console.log(contact);
+  if (!contact) {
+    throw createHttpError(404, `Contact with id ${contactId} not found`);
+  }
 
-    email: {
-      type: String,
-    },
-    isFavourite: {
-      type: Boolean,
-      default: false,
-    },
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
 
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
-);
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+  if (!req.body.name || !req.body.phoneNumber || !req.body.contactType) {
+    throw createHttpError(
+      400,
+      'Name, phoneNumber, and contactType are required fields!',
+    );
+  }
 
-export const ContactsCollection = model('contacts', contactSchema);
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
+    data: contact,
+  });
+};
+
+export const deleteContactController = async (req, res) => {
+  const { contactId } = req.params;
+
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.status(204).send();
+};
+
+export const patchContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+  });
+};
